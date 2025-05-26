db2b1c6c994f63ce9e68762b78fa1e3bb2826cfc
Gaurav Rai <gauravrai3133@gmail.com>
2025-05-27 00:13:58 +0530

    Superadmin Dashbaord card

--- Frontend/app/(main)/superadmin/dashboard/page.tsx
+++ Frontend/app/(main)/superadmin/dashboard/page.tsx
@@ -1,11 +1,44 @@
-
-// app/page.tsx
-"use client";
+import Card from "@/app/components/Card";
+import { FaDonate, FaPlaceOfWorship, FaUsers } from "react-icons/fa";
 
 export default function Page() {
   return (
     <div className="home-container">
-      <h1>Welcome Super Admin. What will you like to do today??</h1>
+      <h1 className="dashboard-title">Welcome Super Admin. What would you like to do today?</h1>
+      <div className="dashboard-grid ">
+        <Card
+          title="Total Donations"
+          value="39,500"
+          icon={<FaDonate />}
+        />
+        <Card
+          title="Temples Registered"
+          value={12}
+          icon={<FaPlaceOfWorship />}
+        />
+        <Card
+          title="Users Registered"
+          value={143}
+          icon={<FaUsers />}
+        />
+      </div>
+      <div className="dashboard-grid ">
+        <Card
+          title="Total Donations"
+          value="39,500"
+          icon={<FaDonate />}
+        />
+        <Card
+          title="Temples Registered"
+          value={12}
+          icon={<FaPlaceOfWorship />}
+        />
+        <Card
+          title="Users Registered"
+          value={143}
+          icon={<FaUsers />}
+        />
+      </div>
     </div>
   );
 }
--- Frontend/app/components/Card.tsx
+++ Frontend/app/components/Card.tsx
@@ -1,16 +1,15 @@
 // components/Card.tsx
 import React from "react";
-import "./styles/sidebar.css"; // Import your CSS styles
+import "./styles/card.css"; // Import your CSS styles
 
 interface CardProps {
   title: string;
   value: string | number;
   icon?: React.ReactNode;
-  details?: string; // Optional details text
 }
 
-const Card: React.FC<CardProps> = ({ title, value, icon, details }) => {
-  return (
+const Card: React.FC<CardProps> = ({ title, value, icon }) => {
+  return (<>
     <div className="card">
       <div className="title">
         <span>{icon}</span>
@@ -18,9 +17,9 @@ const Card: React.FC<CardProps> = ({ title, value, icon, details }) => {
       </div>
       <div className="data">
         <p>{value}</p>
-        {details && <small className="details-text">{details}</small>}
       </div>
     </div>
+    </>
   );
 };
 

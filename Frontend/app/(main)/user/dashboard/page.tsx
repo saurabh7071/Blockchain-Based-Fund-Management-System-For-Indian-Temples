98f8f9a39743bc23606cbbeaeaf719261e321fe0
Saurabh Vaidya <vaidyasaurabh48@gmail.com>
2025-05-28 16:40:49 +0530

    Add temple admin registration confirmation and enhance user login functionality

---
+++ Frontend/app/(main)/user/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// import AuthWrapper from "@/app/components/AuthWrapper";
+
+const DashboardPage = () => {
+    return (
+        
+            <div className="home-container">
+                <h1>Welcome User. What will you like to do today??</h1>
+            </div>
+        
+    );
+};
+
+export default DashboardPage;
